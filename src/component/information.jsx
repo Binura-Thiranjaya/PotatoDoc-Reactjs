905078bf0e6a045a29188052c315eac1ed50216d
Binura Thiranjaya <binura.thiranjaya@nichelabs.lk>
2023-05-31 20:35:04 +0530

    info page done

--- src/component/information.jsx
+++ src/component/information.jsx
@@ -4,7 +4,7 @@ import PotatoImage from "../assets/image/info.png";
 export default function information() {
   return (
     <div>
-      <div className="container">
+      <div className="container pb-5">
         <div className="info-title">Information</div>
         <div className="row">
           <div className="col-md-4">
@@ -37,20 +37,174 @@ export default function information() {
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="dropdown">
-            <button
-              className="btn btn-secondary dropdown-toggle shadow-none"
-              type="button"
-              id="dropdownMenuButton1"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Treatments
-            </button>
-            <div>
-              <div className="climaticrequirements">
-
+        <div className="details">
+          {/*Climatic requirements/ Areas suitable for cultivation */}
+          <div className="row  ">
+            <p>
+              <button
+                class="btn btn-primary btn-block"
+                type="button"
+                data-toggle="collapse"
+                data-target="#climaticRequirements"
+                aria-expanded="false"
+                aria-controls="climaticRequirements"
+              >
+                Climatic requirements/ Areas suitable for cultivation
+              </button>
+            </p>
+            <div class="collapse" id="climaticRequirements">
+              <div class="card card-body">
+                Tuber initiation of potato requires low soil temperature less
+                than 24 oC. Day and night temperature difference should have
+                more than 10 oC for successful crop. Potato grows well in up
+                country area whereas heat tolerance varieties can be grown in
+                dry areas such as Jaffna and Kalpitiya.
+              </div>
+            </div>
+          </div>
+          {/* Soil */}
+          <div className="row  ">
+            <p>
+              <button
+                class="btn btn-primary btn-block"
+                type="button"
+                data-toggle="collapse"
+                data-target="#soil"
+                aria-expanded="false"
+                aria-controls="soil"
+              >
+                Soil
+              </button>
+            </p>
+            <div class="collapse" id="soil">
+              <div class="card card-body">
+                Well drain soil with more organic matter is required. Suitable
+                pH range is 5.5-6.6.
+              </div>
+            </div>
+          </div>
+          {/*Seed requirement*/}
+          <div className="row">
+            <p>
+              <button
+                class="btn btn-primary btn-block"
+                type="button"
+                data-toggle="collapse"
+                data-target="#seed"
+                aria-expanded="false"
+                aria-controls="seed"
+              >
+                Seed requirement
+              </button>
+            </p>
+            <div class="collapse" id="seed">
+              <div class="card card-body">2000 kg tubers /ha</div>
+            </div>
+          </div>
+          {/* Land preparation */}
+          <div className="row">
+            <p>
+              <button
+                class="btn btn-primary btn-block"
+                type="button"
+                data-toggle="collapse"
+                data-target="#land"
+                aria-expanded="false"
+                aria-controls="land"
+              >
+                Land preparation
+              </button>
+            </p>
+            <div class="collapse" id="land">
+              <div class="card card-body">
+                Plough soil to the depth of 30 cm and add lime if pH is low.
+              </div>
+            </div>
+          </div>
+          {/*Planting*/}
+          <div className="row">
+            <p>
+              <button
+                class="btn btn-primary btn-block"
+                type="button"
+                data-toggle="collapse"
+                data-target="#planting"
+                aria-expanded="false"
+                aria-controls="planting"
+              >
+                Planting
+              </button>
+            </p>
+            <div class="collapse" id="planting">
+              <div class="card card-body">
+                Potato tubers can be planted in ridges or raised beds. Soil
+                earthing up is carried out 30 days after planting
+              </div>
+            </div>
+          </div>
+          {/* Spacing */}
+          <div className="row">
+            <p>
+              <button
+                class="btn btn-primary btn-block"
+                type="button"
+                data-toggle="collapse"
+                data-target="#spacing
+                "
+                aria-expanded="false"
+                aria-controls="spacing
+                "
+              >
+                Soil
+              </button>
+            </p>
+            <div class="collapse" id="spacing">
+              <div class="card card-body">60 x 25 cm</div>
+            </div>
+          </div>
+          {/*Water supply */}
+          <div className="row">
+            <p>
+              <button
+                class="btn btn-primary btn-block"
+                type="button"
+                data-toggle="collapse"
+                data-target="#water"
+                aria-expanded="false"
+                aria-controls="water"
+              >
+                Water supply
+              </button>
+            </p>
+            <div class="collapse" id="water">
+              <div class="card card-body">
+                Tuberization and tuber bulking stages are critical for water
+                supply. High yielding crop required about 70 % available water.
+                Sprinkler or drip irrigation methods are more suitable than the
+                furrow irrigation.
+              </div>
+            </div>
+          </div>
+          {/* Weed Control
+           */}
+          <div className="row">
+            <p>
+              <button
+                class="btn btn-primary btn-block"
+                type="button"
+                data-toggle="collapse"
+                data-target="#weed"
+                aria-expanded="false"
+                aria-controls="weed"
+              >
+                Weed Control
+              </button>
+            </p>
+            <div class="collapse" id="weed">
+              <div class="card card-body">
+                Pre-emergent herbicide: metribuzine can be spray to control
+                weeds. Weeds also control by intercultural operations at 2 and 4
+                weeks after planting.
               </div>
             </div>
           </div>
