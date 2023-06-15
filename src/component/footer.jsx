05d23915d8a48c7237f6c9849e778b4ce8309210
Binura Thiranjaya <binura.thiranjaya@nichelabs.lk>
2023-06-15 22:48:13 +0530

    add treatments api to potodoc engine

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -74,7 +74,7 @@ export default function footer() {
       </div>
       <div className="pt-3 pb-1 bg-danger">
         <p className="text-center h6">
-          Project By Binura Thiranjaya @2023 Version: 1.5
+          Project By Binura Thiranjaya @2023 Version: 1.6
         </p>
       </div>
     </div>
--- src/component/potatodoc.jsx
+++ src/component/potatodoc.jsx
@@ -5,7 +5,9 @@ import "./css/potatodoc.css";
 export default function Potatodoc() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [loader, setLoader] = useState(false);
-
+  const [treatmentLoader, setTreatmentLoader] = useState(false);
+  //Store the Treatments
+  const [treatments, setTreatments] = useState(null);
   //Store the Prediction
   const [predictionTagName, setPredictionTagName] = useState(null);
   const [predictionProbability, setPredictionProbability] = useState(null);
@@ -18,8 +20,31 @@ export default function Potatodoc() {
       fileInput.click();
     });
   }
+  const handleTreatments = async (value) => {
+    console.log(value);
+    try {
+      setLoader(true);
+      const number = getTreatmentNumber(value);
+      if (number === 0) {
+        setLoader(false);
+        setTreatmentLoader(false);
+        return;
+      }
+      const backendUrl = process.env.REACT_APP_BACKEND_API;
+      const treatmentUrl = `${backendUrl}disease/${number}`;
+      const response = await fetch(treatmentUrl);
+      const data = await response.json();
+      setTreatments(data);
+      console.log(data);
+      setLoader(false);
+      setTreatmentLoader(true);
+    } catch (error) {
+      console.log(error);
+      setLoader(false);
+      setTreatmentLoader(false);
+    }
+  };
   const handlePredictUsingURL = async (value) => {
-    //console.log(value);
     const predictionUrl = process.env.REACT_APP_PREDICTION_URL_URL;
     const predictionKey = process.env.REACT_APP_PREDICTION_KEY;
     const contentType = "application/json";
@@ -37,8 +62,23 @@ export default function Potatodoc() {
       });
       const data = await response.json();
       console.log(data);
+      const prediction = data.predictions.reduce(
+        (acc, curr) => {
+          return acc.probability > curr.probability ? acc : curr;
+        },
+        { probability: -1 }
+      );
 
-      setLoader(false);
+      setPredictionTagName(prediction.tagName);
+      setPredictionProbability(prediction.probability);
+      // Display the prediction
+      console.log(
+        `The image is most likely "${
+          prediction.tagName
+        }" with a probability of ${Math.round(prediction.probability * 100)}%.`
+      );
+
+      handleTreatments(prediction.tagName);
     } catch (error) {
       console.log(error);
       setLoader(false);
@@ -47,7 +87,7 @@ export default function Potatodoc() {
 
   const handlePredictUsingFile = async (value) => {
     const predictionKey = process.env.REACT_APP_PREDICTION_KEY;
-    const predictionUrl = process.env.REACT_APP_PREDICTION_IMAGE_URL;//process.env.REACT_APP_PREDICTION_IMAGE_URL
+    const predictionUrl = process.env.REACT_APP_PREDICTION_IMAGE_URL; //process.env.REACT_APP_PREDICTION_IMAGE_URL
     const contentType = "application/octet-stream";
     console.log(value);
 
@@ -81,14 +121,22 @@ export default function Potatodoc() {
           prediction.tagName
         }" with a probability of ${Math.round(prediction.probability * 100)}%.`
       );
-
-      setLoader(false);
+      handleTreatments(prediction.tagName);
     } catch (error) {
       console.log(error);
       setLoader(false);
     }
   };
-
+  const getTreatmentNumber = (value) => {
+    switch (value) {
+      case "Early Blight":
+        return 1;
+      case "Late Blight":
+        return 2;
+      default:
+        return 0;
+    }
+  };
   const handleFileInputChange = (event) => {
     const file = event.target.files[0];
     if (file) {
@@ -173,8 +221,8 @@ export default function Potatodoc() {
           </div>
         ) : (
           <div>
-            <div className="d-flex justify-content-center pt-3 pb-3 ">
-              <div className="image-upload rounded d-flex justify-content-center align-items-center flex-column">
+            <div className="d-grid justify-content-center pt-3 pb-3 ">
+              <div className="image-upload  rounded d-flex justify-content-center align-items-center flex-column">
                 <label
                   htmlFor="image-upload"
                   id="upload-icon"
@@ -203,7 +251,7 @@ export default function Potatodoc() {
                   {predictionTagName && predictionProbability ? (
                     <div className="prediction-result">
                       <div className="prediction-probability">
-                        {Math.round(predictionProbability * 100)}% 
+                        {Math.round(predictionProbability * 100)}%
                       </div>
                       <div className="prediction-tag-name p-2">
                         {predictionTagName}
@@ -215,14 +263,94 @@ export default function Potatodoc() {
                     </div>
                   )}
                 </div>
-                {/* <div className="treatments">
-                  <div className="treatment-title">Treatments</div>
-                  <div className="treatment-subtitle">
-                    <a href="https://www.agric.wa.gov.au/pest-insects/potato-leaf-diseases">Potato Leaf Diseases</a>
-                    </div>
-                </div> */}
               </div>
+              <hr />           
             </div>
+            <div className="">
+                {treatmentLoader ? (
+                  // print hello world
+                  <div className="treatments">
+                    <div className="treatments-heading">Treatments</div>
+                    <div className="treatments-content">
+                      <p className="treatments-title">{treatments.name}</p>
+                      <div className="treatments-details">
+                        <div className="d-flex justify-content-center">
+                          <div className="col-md-6">
+                            <div className="treatments-image">
+                              <img
+                                src={treatments.images[0].image}
+                                alt="Treatment"
+                                className="img-fluid"
+                                width="400px"
+                                style={{ height: "400px" }}
+                              />
+                            </div>
+                          </div>
+                          <div className="col-md-6">
+                            <div className="treatments-image">
+                              <img
+                                src={treatments.images[1].image}
+                                alt="Treatment"
+                                className="img-fluid"
+                                width="400px"
+                                style={{ height: "400px" }}
+                              />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="d-flex pt-3 pb-2">
+
+                        <div className="col-md-6">
+                        <div className="treatment-method">
+                          <p className="treatments-subtopic">1. Treatment Methods</p>
+                          {treatments.treatments.map((treatment, index) => {
+                            return (
+                              <div
+                                className="treatment-list"
+                                key={index}
+                              >
+                                <ul>
+                                  <li>
+                                  <div className="treatment-title">
+                                  {treatment.treatment}
+                                </div>
+                                  </li>
+                                </ul>
+                                
+                              </div>
+                            );
+                          })}
+                        </div>
+                        </div>
+                        <div className="col-md-6">
+                        <div className="treatment-fungicides">
+                          <p className="treatments-subtopic">2. Fungicides</p>
+                          {treatments.fungicides.map((fungicide, index) => {
+                            return (
+                              <div
+                                className="treatment-list"
+                                key={index}
+                              >
+                                <ul>
+                                  <li>
+                                  <div className="treatment-title">
+                                  {fungicide.fungicide}
+                                  </div>
+                                  </li>
+                                </ul>
+                                </div>
+                            );
+                          })}
+                        </div>
+                        </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <div></div>
+                )}
+              </div>
             <div className="image-view pb-3 pt-5">
               <div className="d-flex justify-content-center">
                 <button
@@ -274,6 +402,7 @@ export default function Potatodoc() {
           </div>
         )}
       </div>
+      {/* if treatmentLoader is true just show if not nothing display */}
     </div>
   );
 }
